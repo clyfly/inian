a254a3aa75ca625c96d5c8697924dff858ab646b
Radinka Putri El Wahyudi <147782260+clyfly@users.noreply.github.com>
2023-12-12 19:25:25 +0700

    Update luch-proton.sh

--- luch-proton.sh
+++ luch-proton.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Copyright cc 2023 sirnewbies
+#Edited By Denwah/Radinka
+# Before u compile make sure u have the proton clang on your directory and make path on that!!
 
 # setup color
 red='\033[0;31m'
@@ -20,7 +22,7 @@ function clean() {
 }
 
 function build_kernel() {
-    export PATH="/home/romiyusnandar/toolchains/proton-clang/bin:$PATH"
+    export PATH="/workspace/gitpod/proton-clang/bin:$PATH"
     make -j$(nproc --all) O=out ARCH=arm64 rad_defconfig
     make -j$(nproc --all) ARCH=arm64 O=out \
                           CC=clang \
@@ -28,15 +30,28 @@ function build_kernel() {
                           CROSS_COMPILE_ARM32=arm-linux-gnueabi-
     if [ -e "$KERN_IMG" ] || [ -e "$KERN_IMG2" ]; then
         echo -e "\n"
-        echo -e "$green << compile kernel success! >> \n$white"
+        echo -e "$green << Rad Kernel Succesfully compile in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)! >> \n$white"
         echo -e "\n"
     else
         echo -e "\n"
-        echo -e "$red << compile kernel failed! >> \n$white"
+        echo -e "$red <<  Rad Kernel failed to compile in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)! >> \n$white"
         echo -e "\n"
     fi
 }
 
+function zupload()
+{
+git clone --depth=1 https://github.com/clyfly/AnyKernel3.git -b master AnyKernel
+cp out/arch/arm64/boot/Image.gz AnyKernel
+cd AnyKernel
+zip -r9 Focalor-rad.zip *
+curl -T Focalor-rad.zip oshi.at && curl -T Focalor-rad.zip temp.sh
+}
+
+function fun()
+{
+figlet -c "Rad Kernel Compiled"
+}
 # execute
 clean
 build_kernel
