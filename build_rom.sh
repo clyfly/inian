0e55d8d1a08e5fbdb70e3cd5b6467788f5836d9d
Radinka Putri El Wahyudi <147782260+clyfly@users.noreply.github.com>
2024-02-08 15:14:59 +0700

    build: horizon

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,12 @@
 # sync rom
-repo init -u https://github.com/alphadroid-project/manifest -b alpha-13 --git-lfs
+repo init -u https://github.com/HorizonDroid-13/manifest.git -b 13
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # Clone Device tree
 https://github.com/Sicantik-Hanya-Gabut/XYZ-DT -b linear device/xiaomi/rosemary
 
-# Cleaning Kernel tree
-cd kernel/xiaomi/rosemary && make clean
-cd -
-
 # build rom
-source build/envsetup.sh
-lunch lineage_rosemary-user
+. build/envsetup.sh
+lunch aosp_rosemary-user
 export TZ=Asia/Jakarta #put before last build command
 make bacon
