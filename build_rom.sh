452251fa76fedeadbee95acf5c1449bf485d24c7
Radinka Putri El Wahyudi <147782260+clyfly@users.noreply.github.com>
2024-02-08 13:54:49 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,16 @@
 # sync rom
-repo init -u https://github.com/crdroidandroid/android.git -b 13.0 --git-lfs
+repo init -u https://github.com/alphadroid-project/manifest -b alpha-13 --git-lfs
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # Clone Device tree
-git clone https://github.com/Sicantik-Hanya-Gabut/DT.git -b rad device/xiaomi/rosemary
+https://github.com/Sicantik-Hanya-Gabut/XYZ-DT -b linear device/xiaomi/rosemary
 
 # Cleaning Kernel tree
-cd kernel/xiaomi/rosemary && make clean && cd../../../
+cd kernel/xiaomi/rosemary && make clean
+cd -
 
 # build rom
 source build/envsetup.sh
 lunch lineage_rosemary-user
 export TZ=Asia/Jakarta #put before last build command
-m bacon
+make bacon
