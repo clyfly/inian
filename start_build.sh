eb0eb0e2b0345ce125b6d5c71ec187cae2186808
Radinka Putri El Wahyudi <147782260+clyfly@users.noreply.github.com>
2024-02-10 13:23:06 +0700

    rosemary: introduce build bot scripts

---
+++ start_build.sh
@@ -0,0 +1,13 @@
+# Setup everything for u
+echo "Create dir for your build"
+mkdir android
+cd android
+
+# Start Build rom just relax
+echo "start your build sir, wait"
+wget -
+chmod +x build_rom.sh
+./build_rom.sh
+
+# Upload build
+wget 
