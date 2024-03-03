1e3f077a7e6782ba5e138b7c34aa9b127a672ed5
Radinka Putri El Wahyudi <147782260+clyfly@users.noreply.github.com>
2024-03-03 18:35:36 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,9 @@
 # sync rom
 repo init -u https://github.com/CherishOS/android_manifest.git -b eleven  --depth=1
+
+# Clone Device tree
+git clone -b 11 https://github.com/heyradrepo/local_manifests .repo/local_manifests
+
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
 
 # Clone Device tree
